a0d0f3896d3cce9b8d823ac79307ded9322be300
ricardoIB123 <ri2003.ib@gmail.com>
2026-01-28 13:36:37 -0500

    actualizacion

--- src/pages/ApiPage.jsx
+++ src/pages/ApiPage.jsx
@@ -5,17 +5,17 @@ export const ApiPage = () => {
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
       .then((res) => res.json())
       .then((data) => {
-        setPokemons(data);
-        console.log("datos", data);
-      });
+        setPokemons(data.results);
+        console.log("datos", data.results);
+      }).catch((error) => console.error("Error de codigo", error));
   }, []);
   return (
     <>
       <ButtonRetroceder />
-      <div>
+      {/* <div> 
         {pokemons?.abilities?.length > 0 && (
           <span>Habilidades: {pokemons.abilities[0].ability.name} </span>
         )}
@@ -24,6 +24,15 @@ export const ApiPage = () => {
         {pokemons?.stats?.length > 0 && (
         <span>Stats: {pokemons.stats[3].base_stat} </span>
       )}
+      </div>*/}
+      <div className="flex flex-col text-2xl">
+        {
+          pokemons?.map((items, index) => (
+            <span key={index}>
+              {items.name}
+            </span>
+          ))
+        }
       </div>
     </>
   );
