c9d8f810ec457580d7ae86781c79a8914bd09a3f
ricardoIB123 <ri2003.ib@gmail.com>
2026-01-30 13:30:35 -0500

    querys

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { MyRoutes } from "./routes/MyRoutes";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import { MyRoutes } from './routes/MyRoutes'
+const queryClient = new QueryClient();
+
 function App() {
-
-
   return (
-    <>
-      <div>
-        <MyRoutes />
-      </div>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <MyRoutes />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/ApiPage.jsx
+++ src/pages/ApiPage.jsx
@@ -1,48 +1,58 @@
-import { useEffect, useState } from "react"
-import { ButtonRetroceder } from "./ui/buttons/buttonRetroceder"
-import axios from "axios"
+import { use, useEffect, useState } from "react";
+import { ButtonRetroceder } from "./ui/buttons/buttonRetroceder";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export const ApiPage = () => {
-    const [apis, setApis] = useState([])
-    const [cargando, setCargando] = useState(true)
+  const [apis, setApis] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
-    useEffect(() => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["consultas a pokemonapi"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=20",
+      ).then((res) => res.json());
+      return res.results;
+    },
+  });
+  if (isLoading) return <span>Cargando...</span>;
+  if (error) return <span>Error... {error.message} </span>;
 
-        axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
-            .then((res) => {
-                setApis(res.data.results)
-                setCargando(false)
-                console.log("data", res)
-            }).catch((error) => console.error("Error en la api", error)); {/* A diferencia de fetch, que tienes que convertir la respuesta en json, axios lo hace automaticamente y tiene una funcion para cargar */ }
+  {
+    /* useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=20")
+      .then((res) => {
+        setApis(res.data.results);
+        setCargando(false);
+        console.log("data", res);
+      })
+      .catch((error) => console.error("Error en la api", error));
+    {
+      /* A diferencia de fetch, que tienes que convertir la respuesta en json, axios lo hace automaticamente y tiene una funcion para cargar */
+  }
 
-            {/* fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+  {
+    /*  fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
             .then((res) => res.json())
             .then((data) => {
                 setApis(data.results);
                 console.log("data", data.results)
-            }).catch((error) => console.error("Error en el codigo", error));*/}
-        
-    }, [])
-    return (
-        <>
-            <ButtonRetroceder />
-            <div className='h-screen bg-amber-300 text-black'>
-                {
-                    cargando ? (
-                        <span>cargando...</span>
-                    ) : (
-                        <section className="flex flex-col pt-20 text-2xl">
-                            {
-                                apis.map((item, index) => (
-                                    <span key={index}> {item.name} </span>
-                                ))
-                            }
-                        </section>
-                    )
-                }
-
-
-            </div>
-        </>
-    )
-}
+            }).catch((error) => console.error("Error en el codigo", error));
+    
+  }, []);* */
+  }
+  return (
+    <>
+      <ButtonRetroceder />
+      <div className="h-screen bg-amber-300 text-black">
+        <section className="flex flex-col pt-20 text-2xl">
+          {data?.map((item, index) => (
+            <span key={index}> {item.name} </span>
+          ))}
+        </section>
+      </div>
+    </>
+  );
+};
